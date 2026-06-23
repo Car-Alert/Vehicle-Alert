acae4ea063cd01c9b8da4e61329a791b6cbe7933
Car-Alert <hidden.gamerb@gmail.com>
2026-06-22 20:34:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,37 +2,32 @@ const botToken = "8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw";
 const chatId = "2105892713";
 
 const params = new URLSearchParams(window.location.search);
-const vehicle = params.get("v") || "Unknown Vehicle";
+const vehicle = params.get("v") || "Grand Vitara";
 
 document.getElementById("vehicleText").innerText =
   "Vehicle: " + vehicle;
 
 function sendAlert(issue) {
 
-  const message =
-`🚨 Vehicle Alert
-Vehicle: {vehicle}
-Issue: {issue}
-Time: {new Date().toLocaleString()};
+  var message =
+    "🚨 Vehicle Alert\n" +
+    "Vehicle: " + vehicle + "\n" +
+    "Issue: " + issue + "\n" +
+    "Time: " + new Date().toLocaleString();
 
-  fetch(
-    "https://api.telegram.org/bot" +
-  (8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw)+
-    "/sendMessage",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      body:
-        "chat_id=" +
-        (2105892713) +
-        "&text=" +
-        (message)
-    }
-  )
-  .then(response => response.json())
-  .then(data => {
+  fetch("https://api.telegram.org/bot" + 8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw + "/sendMessage", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body:
+      "chat_id=" + encodeURIComponent(2105892713) +
+      "&text=" + encodeURIComponent(hiiii)
+  })
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
 
     if (data.ok) {
       alert("Alert Sent Successfully");
@@ -41,9 +36,11 @@ Time: {new Date().toLocaleString()};
     }
 
   })
-  .catch(error => {
+  .catch(function(error) {
     alert("Network Error");
     console.log(error);
   });
 
 }
+   
+
