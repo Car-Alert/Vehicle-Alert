fc8a831517ceffb0be6fd2df2d84fbfd2b940288
Car-Alert <hidden.gamerb@gmail.com>
2026-06-23 03:46:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,76 @@
-function sendAlert(issue) {
+let selectedIssue = "";
 
-  const botToken = "8078122204:AAEiHYxdsX92FJx-dIzXbl2FLz8gucB9JPc";
+function selectIssue(button, issue) {
+
+  selectedIssue = issue;
+
+  document.querySelectorAll(".opt").forEach(btn => {
+    btn.classList.remove("active");
+  });
+
+  button.classList.add("active");
+
+  const sendBtn = document.getElementById("sendBtn");
+  sendBtn.disabled = false;
+  sendBtn.classList.add("active");
+}
+
+function sendAlert() {
+
+  if (!selectedIssue) {
+    alert("Please select an issue");
+    return;
+  }
+
+  const botToken = "8078122204:AAEItG8nXVq6mTc6JBx3Kz2aHjctouYGQlQ";
   const chatId = "2105892713";
 
-  const vehicle = new URLSearchParams(window.location.search).get("v") || "Unknown";
+  const vehicle =
+    new URLSearchParams(window.location.search).get("v") ||
+    "GRAND VITARA";
 
   const message =
 `🚨 Vehicle Alert
+
 Vehicle: ${vehicle}
-Issue: ${issue}
+
+Issue: ${selectedIssue}
+
 Time: ${new Date().toLocaleString()}`;
 
-// IMPORTANT: form-data style request (works everywhere)
-fetch(`https://api.telegram.org/bot8078122204:AAEiHYxdsX92FJx-dIzXbl2FLz8gucB9JPc/sendMessages`, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: `chat_id=${2105892713}&text=${encodeURIComponent(message)}`
-})
-.then(res => res.json())
-.then(data => {
-  console.log(data);
+  fetch(`https://api.telegram.org/bot${8078122204:AAEItG8nXVq6mTc6JBx3Kz2aHjctouYGQlQ}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body:
+      `chat_id=${2105892713}&text=${encodeURIComponent(message)}`
+  })
+  .then(res => res.json())
+  .then(data => {
 
-  if (data.ok) {
-    alert("Alert Sent Successfully 👍");
-  } else {
-    alert("Telegram Error: " + data.description);
-  }
-})
-.catch(err => {
-  console.log(err);
-  alert("Network Error");
-});
+    console.log(data);
+
+    if (data.ok) {
+
+      document.getElementById("success").style.display = "flex";
+
+      setTimeout(() => {
+        location.reload();
+      }, 3000);
+
+    } else {
+
+      alert("Telegram Error: " + data.description);
+
+    }
+
+  })
+  .catch(err => {
+
+    console.log(err);
+
+    alert("Network Error");
+
+  });
 }
