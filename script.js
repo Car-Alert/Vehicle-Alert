fdbc12c5b82cd16fd73a9073f076aaa60ef249e4
Car-Alert <hidden.gamerb@gmail.com>
2026-06-22 20:19:32 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-alert("JavaScript Loaded");
+const botToken = "8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw";
+const chatId = "2105892713";
+
+const params = new URLSearchParams(window.location.search);
+const vehicle = params.get("v") || "Unknown Vehicle";
+
+document.getElementById("vehicleText").innerText =
+  "Vehicle: " + vehicle;
+
 function sendAlert(issue) {
-
-  const botToken = "8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw";
-  const chatId = "2105892713";
-
-  const vehicle = new URLSearchParams(window.location.search).get("v") || "Unknown";
 
   const message =
 `🚨 Vehicle Alert
@@ -12,26 +15,35 @@ Vehicle: ${vehicle}
 Issue: ${issue}
 Time: ${new Date().toLocaleString()}`;
 
+  fetch(
+    "https://api.telegram.org/bot" +
+    8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw+
+    "/sendMessage",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      body:
+        "chat_id=" +
+        (2105892713) +
+        "&text=" +
+        (message)
+    }
+  )
+  .then(response => response.json())
+  .then(data => {
 
-fetch(`https://api.telegram.org/bot${8078122204:AAGulJMo5Cb71XvWUdm7rV3d0bElEuXY3kw}/sendMessage`, {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/x-www-form-urlencoded"
-  },
-  body: `chat_id=${2105892713}&text=${encodeURIComponent(message)}`
-})
-.then(res => res.json())
-.then(data => {
-  console.log(data);
+    if (data.ok) {
+      alert("Alert Sent Successfully");
+    } else {
+      alert("Telegram Error: " + data.description);
+    }
 
-  if (data.ok) {
-    alert("Alert Sent Successfully 👍");
-  } else {
-    alert("Telegram Error: " + data.description);
-  }
-})
-.catch(err => {
-  console.log(err);
-  alert("Network Error");
-});
+  })
+  .catch(error => {
+    alert("Network Error");
+    console.log(error);
+  });
+
 }
