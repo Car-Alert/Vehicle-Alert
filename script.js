23074aa01cec8cd7d8f6c6e040f4fb890227f08b
Car-Alert <hidden.gamerb@gmail.com>
2026-07-09 09:16:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ function confirmSend(){
 function sendTelegram() {
 
     const BOT_TOKEN = "8078122204:AAHFTan8c_tsAG1QZ3cdRk_tI33E9_hjybw";
+
     const CHAT_ID = "2105892713";
 
     const sendBtn = document.getElementById("sendBtn");
