c406fbc4eaf0a06cbd950ae0182e1ef616749188
Car-Alert <hidden.gamerb@gmail.com>
2026-07-14 00:11:59 +0530

    Update admin.js

--- admin.js
+++ admin.js
@@ -212,8 +212,8 @@ async function downloadSticker() {
     // QR Position
     ctx.drawImage(
         qr,
-        800,      // X
-        500,      // Y
+        700,      // X
+        600,      // Y
         540,      // Width
         540       // Height
     );
